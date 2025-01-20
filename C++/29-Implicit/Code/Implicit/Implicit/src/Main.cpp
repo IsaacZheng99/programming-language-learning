2ad3191b599cd7cde883d5b70e24120724b23953
IsaacZheng99 <isaaczheng.cs@gmail.com>
2025-01-20 23:19:40 +0800

    29-Implicit

---
+++ C++/29-Implicit/Code/Implicit/Implicit/src/Main.cpp
@@ -0,0 +1,150 @@
+#include <iostream>
+
+// 1. one implicit conversion
+// 1.1 create objects with implicit conversion
+class Entity
+{
+private:
+	std::string m_Name;
+	int m_Age;
+public:
+	Entity(const std::string& name)
+		: m_Name(name), m_Age(-1) {
+	}
+
+	Entity(int age)
+		: m_Name("Unknown"), m_Age(age) {
+	}
+};
+
+void Function(const Entity& e)
+{
+
+}
+
+// 1.3 type conversion operator
+class MyClass
+{
+private:
+	int m_Value;
+public:
+	MyClass()
+		: m_Value(1) {
+	}
+
+	operator int() const
+	{
+		return m_Value;
+	}
+};
+
+// 2. explicit
+class EntityWithExplicit
+{
+private:
+	std::string m_Name;
+	int m_Age;
+public:
+	EntityWithExplicit(const std::string name)
+		: m_Name(name), m_Age(-1) {
+	}
+
+	explicit EntityWithExplicit(int age)
+		: m_Name("Unknown"), m_Age(age) {
+	}
+
+	int GetAge() const
+	{
+		return m_Age;
+	}
+};
+
+
+int main()
+{
+	// 1. one implicit conversion
+	// 1.1 implicit construction
+	Entity e1 = Entity("xiaoli");
+	Entity e2 = (std::string)"xiaoli";
+	//Entity e3 = "xiaoli";  // Wrong! error C2440: 'initializing': cannot convert from 'const char [7]' to 'Entity'
+	Entity e4 = Entity(25);
+	Entity e5 = 25;
+
+	Function((std::string)"xiaoli");
+
+	// 1.2 arithmetic conversion
+	// integrl promotion
+	char c1 = 10;
+	int i1 = 20;
+	int result1 = c1 + i1;
+	std::cout << c1 + i1 << std::endl;
+	std::cout << typeid(c1 + i1).name() << std::endl;
+	std::cout << result1 << std::endl;
+	std::cout << typeid(result1).name() << std::endl;
+	// usual arithmetic conversion
+	int i2 = 12;
+	float f1 = 1.1f;
+	std::cout << i2 + f1 << std::endl;  // float priority
+	std::cout << typeid(i2 + f1).name() << std::endl;
+	std::cout << i2 / f1 << std::endl;
+	std::cout << typeid(i2 / f1).name() << std::endl;
+	std::cout << i2 / i1 << std::endl;
+	std::cout << typeid(i2 / i1).name() << std::endl;
+	std::cout << i1 / i2 << std::endl;
+	std::cout << typeid(i1 / i2).name() << std::endl;
+	int i3 = 120;
+	std::cout << i3 / i2 << std::endl;
+	std::cout << typeid(i3 / i2).name() << std::endl;
+	double d1 = 2.2;
+	std::cout << i2 + d1 << std::endl;
+	std::cout << typeid(i2 + d1).name() << std::endl;
+	std::cout << f1 + d1 << std::endl;
+	std::cout << typeid(f1 + d1).name() << std::endl;
+
+	int i4 = 1;
+	long l1 = 10;
+	std::cout << i4 + l1 << std::endl;  // larger-ranged number priority
+	std::cout << typeid(i4 + l1).name() << std::endl;
+	unsigned int ui1 = 100;
+	std::cout << i4 + ui1 << std::endl;
+	std::cout << typeid(i4 + ui1).name() << std::endl;
+
+	bool b1 = true;
+	bool b2 = false;
+	int i5 = 99;
+	std::cout << i5 + b1 << std::endl;  // bool conversion
+	std::cout << typeid(i5 + b1).name() << std::endl;
+	std::cout << i5 + b2 << std::endl;
+	std::cout << typeid(i5 + b2).name() << std::endl;
+
+	// floating-point promotion
+	float f2 = 3.3f;
+	double d2 = 4.4;
+	std::cout << f2 + d2 << std::endl;
+	std::cout << typeid(f2 + d2).name() << std::endl;
+
+	// 1.3 assignment conversion
+	float f3 = 1.1f;
+	double d3 = f3;
+	char c2 = 1;
+	int i6 = c2;
+	char c3 = i6;
+
+	// 1.4 type conversion operator
+	MyClass obj;
+	int a = obj;
+	std::cout << a << std::endl;
+
+	// 2. explicit
+	//EntityWithExplicit ee1 = 25;  // Wrong!
+	EntityWithExplicit ee2 = EntityWithExplicit(25);
+	std::cout << ee2.GetAge() << std::endl;
+	EntityWithExplicit ee3(25);
+	std::cout << ee2.GetAge() << std::endl;
+	EntityWithExplicit ee4 = (EntityWithExplicit)25;
+	std::cout << ee2.GetAge() << std::endl;
+	EntityWithExplicit ee5 = EntityWithExplicit(25);
+	std::cout << ee2.GetAge() << std::endl;
+	EntityWithExplicit ee6 = (EntityWithExplicit)(25);
+	std::cout << ee2.GetAge() << std::endl;
+}
