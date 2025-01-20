7818741d1348259815a403246a557dbea279a88d
IsaacZheng99 <isaaczheng.cs@gmail.com>
2025-01-20 23:27:24 +0800

    29-add comments

--- C++/29-Implicit/Code/Implicit/Implicit/src/Main.cpp
+++ C++/29-Implicit/Code/Implicit/Implicit/src/Main.cpp
@@ -22,7 +22,7 @@ void Function(const Entity& e)
 
 }
 
-// 1.3 type conversion operator
+// 1.4 type conversion operator
 class MyClass
 {
 private:
@@ -63,7 +63,9 @@ public:
 int main()
 {
 	// 1. one implicit conversion
+	std::cout << "-----1. one implicit conversion-----" << std::endl;
 	// 1.1 implicit construction
+	std::cout << "-----1.1 implicit construction-----" << std::endl;
 	Entity e1 = Entity("xiaoli");
 	Entity e2 = (std::string)"xiaoli";
 	//Entity e3 = "xiaoli";  // Wrong! error C2440: 'initializing': cannot convert from 'const char [7]' to 'Entity'
@@ -73,7 +75,9 @@ int main()
 	Function((std::string)"xiaoli");
 
 	// 1.2 arithmetic conversion
+	std::cout << "-----1.2 arithmetic conversion-----" << std::endl;
 	// integrl promotion
+	std::cout << "-----integrl promotion-----" << std::endl;
 	char c1 = 10;
 	int i1 = 20;
 	int result1 = c1 + i1;
@@ -82,6 +86,8 @@ int main()
 	std::cout << result1 << std::endl;
 	std::cout << typeid(result1).name() << std::endl;
 	// usual arithmetic conversion
+	std::cout << "-----usual arithmetic conversion-----" << std::endl;
+	std::cout << "-----float priority-----" << std::endl;
 	int i2 = 12;
 	float f1 = 1.1f;
 	std::cout << i2 + f1 << std::endl;  // float priority
@@ -100,7 +106,8 @@ int main()
 	std::cout << typeid(i2 + d1).name() << std::endl;
 	std::cout << f1 + d1 << std::endl;
 	std::cout << typeid(f1 + d1).name() << std::endl;
-
+	
+	std::cout << "-----larger-ranged number priority-----" << std::endl;
 	int i4 = 1;
 	long l1 = 10;
 	std::cout << i4 + l1 << std::endl;  // larger-ranged number priority
@@ -109,6 +116,7 @@ int main()
 	std::cout << i4 + ui1 << std::endl;
 	std::cout << typeid(i4 + ui1).name() << std::endl;
 
+	std::cout << "-----bool conversion-----" << std::endl;
 	bool b1 = true;
 	bool b2 = false;
 	int i5 = 99;
@@ -118,12 +126,14 @@ int main()
 	std::cout << typeid(i5 + b2).name() << std::endl;
 
 	// floating-point promotion
+	std::cout << "-----floating-point promotion-----" << std::endl;
 	float f2 = 3.3f;
 	double d2 = 4.4;
 	std::cout << f2 + d2 << std::endl;
 	std::cout << typeid(f2 + d2).name() << std::endl;
 
 	// 1.3 assignment conversion
+	std::cout << "-----1.3 assignment conversion-----" << std::endl;
 	float f3 = 1.1f;
 	double d3 = f3;
 	char c2 = 1;
@@ -131,11 +141,13 @@ int main()
 	char c3 = i6;
 
 	// 1.4 type conversion operator
+	std::cout << "-----1.4 type conversion operator-----" << std::endl;
 	MyClass obj;
 	int a = obj;
 	std::cout << a << std::endl;
 
 	// 2. explicit
+	std::cout << "-----2. explicit-----" << std::endl;
 	//EntityWithExplicit ee1 = 25;  // Wrong!
 	EntityWithExplicit ee2 = EntityWithExplicit(25);
 	std::cout << ee2.GetAge() << std::endl;
