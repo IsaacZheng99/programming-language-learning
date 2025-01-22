19d2c734567ebd37670561fb107cb6cd391ebdab
IsaacZheng99 <isaaczheng.cs@gmail.com>
2025-01-22 16:15:33 +0800

    38-Code

---
+++ C++/38-Create_Library_used_by_Multiple_Projects/Code/Game/Engine/src/Engine.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+#include "Engine.h"
+
+
+namespace Engine {
+
+	void PrintMessage()
+	{
+		std::cout << "Hello World!" << std::endl;
+	}
+}
---
+++ C++/38-Create_Library_used_by_Multiple_Projects/Code/Game/Game/src/Application.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+// 2.3 set Additional Include Directories
+//#include "../../Engine/src/Engine.h"
+#include "Engine.h"
+
+
+int main()
+{
+	Engine::PrintMessage();
+
+	std::cin.get();
+}
