801d682556c70e8c0de07450b4a3631708503c1e
IsaacZheng99 <isaaczheng.cs@gmail.com>
2025-02-07 11:09:08 +0800

    49-Code

---
+++ C++/49-Thread/Code/Thread/Thread/src/Main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <thread>
+#include <chrono>
+
+
+static bool s_Finished = false;
+
+
+void DoWork()
+{
+	using namespace std::literals::chrono_literals;
+
+	std::cout << "Started thread id = " << std::this_thread::get_id() << std::endl;
+
+	while (!s_Finished)
+	{
+		std::cout << "Working...\n";
+		std::this_thread::sleep_for(1s);
+	}
+}
+
+
+int main()
+{
+	std::thread worker(DoWork);
+
+	std::cin.get();  // manually block the main thread
+	s_Finished = true;
+
+	worker.join();
+
+	std::cout << "Finished!" << std::endl;
+	std::cout << "Started therad id = " << std::this_thread::get_id() << std::endl;
+}
